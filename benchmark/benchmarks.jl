c4383db7101971c828f752a6d08781b850321db1
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-05-13 12:14:36 -0400

    Add benchmarking

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,26 @@
+### These benchmarks can be run from the top-level directory of this repo with
+###
+###     julia -e 'using PkgBenchmark; results=benchmarkpkg("Spherical"); export_markdown("benchmark/results.md", results)'
+###
+### This runs the benchmarks (possibly tuning them automatically first), and writes the results to a nice markdown file.
+
+using BenchmarkTools
+using Spherical
+
+const SUITE = BenchmarkGroup()
+
+SUITE["complex_powers"] = BenchmarkGroup(["recursions", "complex"])
+for T in [big, Float64, Float32, Float16]
+    z = exp(T(6)im/5)
+    m = 10_000
+    zpowers = Array{typeof(z)}(undef, m+1)
+    SUITE["complex_powers"][T] = @benchmarkable complex_powers!($zpowers, $z)
+end
+
+
+# SUITE["trigonometry"]["hyperbolic"] = BenchmarkGroup()
+# for f in (sin, cos, tan)
+#     for x in (0.0, pi)
+#         SUITE["trigonometry"]["hyperbolic"][string(f), x] = @benchmarkable ($f)($x)
+#     end
+# end
