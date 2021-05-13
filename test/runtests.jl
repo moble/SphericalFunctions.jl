30018d7bba133904af9303d8892c41775bacb2d9
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-05-13 14:53:42 -0400

    Check that complex_powers throws error when abs(z)!=1

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,11 +9,14 @@ using Test
     )
 
     for T in [Float64, Float32, Float16]
+        fudge = one(T) + 2 * sqrt(eps(T))
         for k in 0:25
             z = exp(k*big(π)im/10)
             for m in [0, 1, 2, 3, 4, 1_000]
                 mine, theirs, ϵ = complex_powers_comparison(z, m, T)
                 @test mine ≈ theirs rtol=2ϵ
+                @test_throws DomainError complex_powers(Complex{T}(z*fudge), m)
+                @test_throws DomainError complex_powers(Complex{T}(z/fudge), m)
             end
         end
     end
