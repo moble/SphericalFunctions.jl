dec07a697c43d4085354010e42a1fa62050d62a2
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-05-18 16:00:07 -0400

    Validate H calculation against python

--- src/wigner.jl
+++ src/wigner.jl
@@ -1,5 +1,8 @@
-struct Wugner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real} end
-function Wugner(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int), T::Type{<:Real}=Float64)
+### TODO: Merge the Wigner and WignerWorkspace types; there's no real reason for the singleton
+
+
+struct Wigner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real} end
+function Wigner(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int), T::Type{<:Real}=Float64)
     if ℓₘᵢₙ < 0
         throw(DomainError("ℓₘᵢₙ = $ℓₘᵢₙ",
                 "This function is only valid for non-negative values of ℓ."
@@ -15,182 +18,60 @@ function Wugner(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int), T::Type{
                 "The input ℓₘᵢₙ must be no greater than the input ℓₘₐₓ=$ℓₘₐₓ."
         ))
     end
-    Wugner{ℓₘᵢₙ, ℓₘₐₓ, min(ℓₘₐₓ, abs(m′ₘₐₓ)), T}
+    Wigner{ℓₘᵢₙ, ℓₘₐₓ, min(ℓₘₐₓ, abs(m′ₘₐₓ)), T}
 end
 
 # Access type parameters
-@generated ℓₘᵢₙ(::Type{W}) where {W<:Wugner} = W.parameters[1]
-@generated ℓₘₐₓ(::Type{W}) where {W<:Wugner} = W.parameters[2]
-@generated m′ₘₐₓ(::Type{W}) where {W<:Wugner} = W.parameters[3]
-@generated T(::Type{W}) where {W<:Wugner} = W.parameters[4]
+@generated ℓₘᵢₙ(::Type{W}) where {W<:Wigner} = W.parameters[1]
+@generated ℓₘₐₓ(::Type{W}) where {W<:Wigner} = W.parameters[2]
+@generated m′ₘₐₓ(::Type{W}) where {W<:Wigner} = W.parameters[3]
+@generated T(::Type{W}) where {W<:Wigner} = W.parameters[4]
 
 # Access pre-computed array sizes
-@generated WignerHsize(::Type{W}) where {W<:Wugner} = WignerHsize(m′ₘₐₓ(W), ℓₘₐₓ(W))
-@generated Wignerdsize(::Type{W}) where {W<:Wugner} = Wignerdsize(ℓₘᵢₙ(W), m′ₘₐₓ(W), ℓₘₐₓ(W))
-@generated WignerDsize(::Type{W}) where {W<:Wugner} = WignerDsize(ℓₘᵢₙ(W), m′ₘₐₓ(W), ℓₘₐₓ(W))
-@generated Ysize(::Type{W}) where {W<:Wugner} = Ysize(ℓₘᵢₙ(W), ℓₘₐₓ(W))
+@generated WignerHsize(::Type{W}) where {W<:Wigner} = WignerHsize(m′ₘₐₓ(W), ℓₘₐₓ(W))
+@generated Wignerdsize(::Type{W}) where {W<:Wigner} = Wignerdsize(ℓₘᵢₙ(W), m′ₘₐₓ(W), ℓₘₐₓ(W))
+@generated WignerDsize(::Type{W}) where {W<:Wigner} = WignerDsize(ℓₘᵢₙ(W), m′ₘₐₓ(W), ℓₘₐₓ(W))
+@generated Ysize(::Type{W}) where {W<:Wigner} = Ysize(ℓₘᵢₙ(W), ℓₘₐₓ(W))
 
 # Access pre-computed helper data
-@generated function a(::Type{W}) where {W<:Wugner}
-    [√((n+1+m) * (n+1-m) / T(W)((2*n+1)*(2*n+3))) for n in 0:ℓₘₐₓ(W)+1 for m in 0:n]
+@generated function a(::Type{W}) where {W<:Wigner}
+    [√((n+1+m) * (n+1-m) / T(W)((2n+1)*(2n+3))) for n in 0:ℓₘₐₓ(W)+1 for m in 0:n]
 end
-@generated function b(::Type{W}) where {W<:Wugner}
-    [(m<0 ? -1 : 1) * √((n-m-1) * (n-m) / T(W)((2*n-1)*(2*n+1))) for n in 0:ℓₘₐₓ+1 for m in -n:n]
+@generated function b(::Type{W}) where {W<:Wigner}
+    [(m<0 ? -1 : 1) * √((n-m-1) * (n-m) / T(W)((2n-1)*(2n+1))) for n in 0:ℓₘₐₓ(W)+1 for m in -n:n]
 end
-@generated function d(::Type{W}) where {W<:Wugner}
-    [(m<0 ? -0.5 : 0.5) * √T(W)((n-m) * (n+m+1)) for n in 0:ℓₘₐₓ+1 for m in -n:n]
+@generated function d(::Type{W}) where {W<:Wigner}
+    [(m<0 ? -1 : 1) * (√T(W)((n-m) * (n+m+1))) / 2 for n in 0:ℓₘₐₓ(W)+1 for m in -n:n]
 end
-@generated function g(::Type{W}) where {W<:Wugner}
-    [2(m+1) / √T(W)((n-m)*(n+m+1)) for n in 0:ℓₘₐₓ+1 for m in -n:n]
+@generated function g(::Type{W}) where {W<:Wigner}
+    [2(m+1) / √T(W)((n-m)*(n+m+1)) for n in 0:ℓₘₐₓ(W)+1 for m in -n:n]
 end
-@generated function h(::Type{W}) where {W<:Wugner}
-    [n==m ? NaN : √((n+m+2)*(n-m-1) / T(W)((n-m)*(n+m+1))) for n in 0:ℓₘₐₓ+1 for m in -n:n]
+@generated function h(::Type{W}) where {W<:Wigner}
+    [n==m ? T(W)(NaN) : √((n+m+2)*(n-m-1) / T(W)((n-m)*(n+m+1))) for n in 0:ℓₘₐₓ(W)+1 for m in -n:n]
 end
 
-
-
-
-
-
-struct Wagner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real}
-    Hsize::Int
-    dsize::Int
-    Dsize::Int
-    Ysize::Int
-    a::Vector{T}
-    b::Vector{T}
-    d::Vector{T}
-    g::Vector{T}
-    h::Vector{T}
-    function Wagner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T}() where {ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real}
-        # Some generators to use below
-        nm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in -n:n)
-        absnm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in 0:n)
-
-        # Pre-compute these useful arrays
-        a = [√((n+1+m) * (n+1-m) / T((2*n+1)*(2*n+3))) for (n, m) in absnm]
-        b = [(m<0 ? -1 : 1) * √((n-m-1) * (n-m) / T((2*n-1)*(2*n+1))) for (n, m) in nm]
-        d = [(m<0 ? -0.5 : 0.5) * √T((n-m) * (n+m+1)) for (n, m) in nm]
-        g = [2(m+1) / √T((n-m)*(n+m+1)) for (n, m) in nm]
-        h = [n==m ? NaN : √((n+m+2)*(n-m-1) / T((n-m)*(n+m+1))) for (n, m) in nm]
-
-        new(
-            WignerHsize(m′ₘₐₓ, ℓₘₐₓ),
-            Wignerdsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-            WignerDsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-            Ysize(ℓₘᵢₙ, ℓₘₐₓ),
-            a, b, d, g, h
+struct WignerWorkspace{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real}
+    W::Type{Wigner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T}}
+    Hwedge::Vector{T}
+    Hv::Vector{T}
+    Hextra::Vector{T}
+    zₐpowers::Vector{Complex{T}}
+    zᵧpowers::Vector{Complex{T}}
+    z::Vector{Complex{T}}
+    function WignerWorkspace(W::Type{Wigner{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T}}) where {ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T<:Real}
+        new{ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T}(W,
+            Array{T}(undef, WignerHsize(W)),
+            Array{T}(undef, (ℓₘₐₓ + 1)^2),
+            Array{T}(undef, ℓₘₐₓ + 2),
+            Array{Complex{T}}(undef, ℓₘₐₓ + 1),
+            Array{Complex{T}}(undef, ℓₘₐₓ + 1),
+            Array{Complex{T}}(undef, 3)
         )
     end
 end
 
-function Wagner(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int), T::Type{<:Real}=Float64)
-    if ℓₘᵢₙ < 0
-        throw(DomainError("ℓₘᵢₙ = $ℓₘᵢₙ",
-                "This function is only valid for non-negative values of ℓ."
-        ))
-    end
-    if ℓₘₐₓ < 0
-        throw(DomainError("ℓₘₐₓ = $ℓₘₐₓ",
-                "This function is only valid for non-negative values of ℓ."
-        ))
-    end
-    if ℓₘᵢₙ > ℓₘₐₓ
-        throw(DomainError("ℓₘᵢₙ = $ℓₘᵢₙ",
-                "The input ℓₘᵢₙ must be no greater than the input ℓₘₐₓ=$ℓₘₐₓ."
-        ))
-    end
-    Wagner{ℓₘᵢₙ, ℓₘₐₓ, min(ℓₘₐₓ, abs(m′ₘₐₓ)), T}()
+function Base.show(io::IO, workspace::WignerWorkspace)
+    print(io, typeof(workspace))
 end
 
-function Base.show(io::IO, w::Wagner)
-    print(io, typeof(w))
-end
-
-
-
-struct Wogner{Lmin, Lmax, Mmax, T<:Real, Hsize, dsize, Dsize, Ysize, a, b, d, g, h}
-end
-
-function Wogner(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int), T::Type{<:Real}=Float64)
-    m′ₘₐₓ = min(ℓₘₐₓ, abs(m′ₘₐₓ))
-
-    # Some generators to use below
-    nm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in -n:n)
-    absnm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in 0:n)
-
-    Wogner{
-        ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ, T,
-        WignerHsize(m′ₘₐₓ, ℓₘₐₓ),
-        WignerDsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-        WignerDsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-        Ysize(ℓₘᵢₙ, ℓₘₐₓ),
-        tuple([√((n+1+m) * (n+1-m) / T((2*n+1)*(2*n+3))) for (n, m) in absnm]...),
-        tuple([(m<0 ? -1 : 1) * √((n-m-1) * (n-m) / T((2*n-1)*(2*n+1))) for (n, m) in nm]...),
-        tuple([(m<0 ? -0.5 : 0.5) * √T((n-m) * (n+m+1)) for (n, m) in nm]...),
-        tuple([2(m+1) / √T((n-m)*(n+m+1)) for (n, m) in nm]...),
-        tuple([n==m ? NaN : √((n+m+2)*(n-m-1) / T((n-m)*(n+m+1))) for (n, m) in nm]...)
-    }()
-end
-
-function Base.show(io::IO, w::Wogner)
-    wtype = typeof(w)
-    print(io, "Wogner($(wtype.parameters[1]), $(wtype.parameters[2]), $(wtype.parameters[3]), $(wtype.parameters[4]))")
-end
-
-struct Wigner{T<:Real}
-    ℓₘᵢₙ::Int
-    ℓₘₐₓ::Int
-    m′ₘₐₓ::Int
-    Hsize::Int
-    dsize::Int
-    Dsize::Int
-    Ysize::Int
-    a::Vector{T}
-    b::Vector{T}
-    d::Vector{T}
-    g::Vector{T}
-    h::Vector{T}
-    function Wigner{T}(ℓₘᵢₙ, ℓₘₐₓ, m′ₘₐₓ=typemax(Int)) where {T<:Real}
-        if ℓₘᵢₙ < 0
-            throw(DomainError("ℓₘᵢₙ = $ℓₘᵢₙ",
-                    "This function is only valid for non-negative values of ℓ."
-            ))
-        end
-        if ℓₘₐₓ < 0
-            throw(DomainError("ℓₘₐₓ = $ℓₘₐₓ",
-                    "This function is only valid for non-negative values of ℓ."
-            ))
-        end
-        if ℓₘᵢₙ > ℓₘₐₓ
-            throw(DomainError("ℓₘᵢₙ = $ℓₘᵢₙ",
-                    "The input ℓₘᵢₙ must be no greater than the input ℓₘₐₓ=$ℓₘₐₓ."
-            ))
-        end
-
-        # Some generators to use below
-        nm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in -n:n)
-        absnm = ((n, m) for n in 0:ℓₘₐₓ+1 for m in 0:n)
-
-        # Pre-compute these useful arrays
-        a = [√((n+1+m) * (n+1-m) / T((2*n+1)*(2*n+3))) for (n, m) in absnm]
-        b = [(m<0 ? -1 : 1) * √((n-m-1) * (n-m) / T((2*n-1)*(2*n+1))) for (n, m) in nm]
-        d = [(m<0 ? -0.5 : 0.5) * √T((n-m) * (n+m+1)) for (n, m) in nm]
-        g = [2(m+1) / √T((n-m)*(n+m+1)) for (n, m) in nm]
-        h = [n==m ? NaN : √((n+m+2)*(n-m-1) / T((n-m)*(n+m+1))) for (n, m) in nm]
-
-        new(
-            ℓₘᵢₙ, ℓₘₐₓ, min(ℓₘₐₓ, abs(m′ₘₐₓ)),
-            WignerHsize(m′ₘₐₓ, ℓₘₐₓ),
-            WignerDsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-            WignerDsize(ℓₘᵢₙ, m′ₘₐₓ, ℓₘₐₓ),
-            Ysize(ℓₘᵢₙ, ℓₘₐₓ),
-            a, b, d, g, h
-        )
-    end
-end
-
-
-function Base.show(io::IO, w::Wigner{T}) where {T<:Real}
-    print(io, "Wigner{$T}($(w.ℓₘᵢₙ), $(w.ℓₘₐₓ), $(w.m′ₘₐₓ))")
-end
+include("wignerHrecursions.jl")
--- src/wignerHrecursions.jl
+++ src/wignerHrecursions.jl
@@ -78,13 +78,13 @@ Assumes array is ordered as
 nmpm_index(n, mp, m) = (((4n + 6) * n + 6mp + 5) * n + 3(m + mp)) ÷ 3 + 1
 
 
-function _step_1(Hwedge):
+@inbounds function _step_1!(w::WignerWorkspace)
     """If n=0 set H_{0}^{0,0}=1."""
-    Hwedge[1] = 1
+    w.Hwedge[1] = 1
 end
 
 
-function _step_2(g, h, n_max, mp_max, Hwedge, Hextra, Hv, expiβ):
+@inbounds function _step_2!(w::WignerWorkspace, expiβ::Complex)
     """Compute values H^{0,m}_{n}(β)for m=0,...,n and H^{0,m}_{n+1}(β) for m=0,...,n+1 using Eq. (32):
 
         H^{0,m}_{n}(β) = (-1)^m √((n-|m|)! / (n+|m|)!) P^{|m|}_{n}(cos β)
@@ -101,64 +101,78 @@ function _step_2(g, h, n_max, mp_max, Hwedge, Hextra, Hv, expiβ):
     from its symmetric equivalent H^{0, 1}_{n} in this step.
 
     """
-    cosβ = expiβ.real
-    sinβ = expiβ.imag
-    if n_max > 0:
+    gvalues, hvalues, n_max, mp_max = g(w.W), h(w.W), ℓₘₐₓ(w.W), m′ₘₐₓ(w.W)
+    Hwedge, Hextra, Hv = w.Hwedge, w.Hextra, w.Hv
+    cosβ = expiβ.re
+    sinβ = expiβ.im
+    TW = T(w.W)
+    sqrt3 = √TW(3)
+    inverse_sqrt2 = inv(√TW(2))
+
+    if n_max > 0
         # n = 1
         n0n_index = WignerHindex(1, 0, 1, mp_max)
         nn_index = nm_index(1, 1)
         Hwedge[n0n_index] = sqrt3  # Un-normalized
-        Hwedge[n0n_index-1] = (g[nn_index-1] * cosβ) * inverse_sqrt2  # Normalized
+        Hwedge[n0n_index-1] = (gvalues[nn_index-1] * cosβ) * inverse_sqrt2  # Normalized
         # n = 2, ..., n_max+1
-        for n in range(2, n_max+2):
-            if n <= n_max:
+        for n in 2:n_max+1
+            if n <= n_max
                 n0n_index = WignerHindex(n, 0, n, mp_max)
                 H = Hwedge
-            else:
-                n0n_index = n
+            else
+                n0n_index = n + 1
                 H = Hextra
+            end
             nm10nm1_index = WignerHindex(n-1, 0, n-1, mp_max)
             nn_index = nm_index(n, n)
-            const = np.sqrt(1.0 + 0.5/n)
-            gi = g[nn_index-1]
+            constant = √(1 + 1/TW(2n))
+            gi = gvalues[nn_index-1]
             # m = n
-            H[n0n_index] = const * Hwedge[nm10nm1_index]
+            H[n0n_index] = constant * Hwedge[nm10nm1_index]
             # m = n-1
             H[n0n_index-1] = gi * cosβ * H[n0n_index]
             # m = n-2, ..., 1
-            for i in range(2, n):
-                gi = g[nn_index-i]
-                hi = h[nn_index-i]
-                H[n0n_index-i] = gi * cosβ * H[n0n_index-i+1] - hi * sinβ**2 * H[n0n_index-i+2]
+            for i in 2:n-1
+                gi = gvalues[nn_index-i]
+                hi = hvalues[nn_index-i]
+                H[n0n_index-i] = gi * cosβ * H[n0n_index-i+1] - hi * sinβ^2 * H[n0n_index-i+2]
+            end
             # m = 0, with normalization
-            const = 1.0 / np.sqrt(4*n+2)
-            gi = g[nn_index-n]
-            hi = h[nn_index-n]
-            H[n0n_index-n] = (gi * cosβ * H[n0n_index-n+1] - hi * sinβ**2 * H[n0n_index-n+2]) * const
+            constant = 1 / √TW(4n+2)
+            gi = gvalues[nn_index-n]
+            hi = hvalues[nn_index-n]
+            H[n0n_index-n] = (gi * cosβ * H[n0n_index-n+1] - hi * sinβ^2 * H[n0n_index-n+2]) * constant
             # Now, loop back through, correcting the normalization for this row, except for n=n element
-            prefactor = const
-            for i in range(1, n):
+            prefactor = constant
+            for i in 1:n-1
                 prefactor *= sinβ
                 H[n0n_index-n+i] *= prefactor
+            end
             # Supply extra edge cases as noted in docstring
-            if n <= n_max:
+            if n <= n_max
                 Hv[nm_index(n, 1)] = Hwedge[WignerHindex(n, 0, 1, mp_max)]
                 Hv[nm_index(n, 0)] = Hwedge[WignerHindex(n, 0, 1, mp_max)]
+            end
+        end
         # Correct normalization of m=n elements
-        prefactor = 1.0
-        for n in range(1, n_max+1):
+        prefactor = one(TW)
+        for n in 1:n_max
             prefactor *= sinβ
-            Hwedge[WignerHindex(n, 0, n, mp_max)] *= prefactor / np.sqrt(4*n+2)
-        for n in [n_max+1]:
+            Hwedge[WignerHindex(n, 0, n, mp_max)] *= prefactor / √TW(4n+2)
+        end
+        for n in [n_max+1]
             prefactor *= sinβ
-            Hextra[n] *= prefactor / np.sqrt(4*n+2)
+            Hextra[n+1] *= prefactor / √TW(4n+2)
+        end
         # Supply extra edge cases as noted in docstring
         Hv[nm_index(1, 1)] = Hwedge[WignerHindex(1, 0, 1, mp_max)]
         Hv[nm_index(1, 0)] = Hwedge[WignerHindex(1, 0, 1, mp_max)]
+    end
 end
 
 
-function _step_3(a, b, n_max, mp_max, Hwedge, Hextra, expiβ):
+@inbounds function _step_3!(w::WignerWorkspace, expiβ::Complex)
     """Use relation (41) to compute H^{1,m}_{n}(β) for m=1,...,n.  Using symmetry and shift
     of the indices this relation can be written as
 
@@ -167,36 +181,47 @@ function _step_3(a, b, n_max, mp_max, Hwedge, Hextra, expiβ):
                                    − a^{m}_{n} sinβ H^{0, m}_{n+1}
 
     """
-    cosβ = expiβ.real
-    sinβ = expiβ.imag
-    if n_max > 0 and mp_max > 0:
-        for n in range(1, n_max+1):
+    avalues = a(w.W)
+    bvalues = b(w.W)
+    n_max = ℓₘₐₓ(w.W)
+    mp_max = m′ₘₐₓ(w.W)
+    Hwedge = w.Hwedge
+    Hextra = w.Hextra
+    cosβ = expiβ.re
+    sinβ = expiβ.im
+
+    if n_max > 0 && mp_max > 0
+        for n in 1:n_max
             # m = 1, ..., n
             i1 = WignerHindex(n, 1, 1, mp_max)
-            if n+1 <= n_max:
+            if n+1 <= n_max
                 i2 = WignerHindex(n+1, 0, 0, mp_max)
                 H2 = Hwedge
-            else:
-                i2 = 0
+            else
+                i2 = 1
                 H2 = Hextra
+            end
             i3 = nm_index(n+1, 0)
             i4 = nabsm_index(n, 1)
-            inverse_b5 = 1.0 / b[i3]
-            for i in range(n):
-                b6 = b[-i+i3-2]
-                b7 = b[i+i3]
-                a8 = a[i+i4]
+            inverse_b5 = inv(bvalues[i3])
+            for i in 0:n-1
+                b6 = bvalues[-i+i3-2]
+                b7 = bvalues[i+i3]
+                a8 = avalues[i+i4]
                 Hwedge[i+i1] = inverse_b5 * (
-                    0.5 * (
+                    (
                           b6 * (1-cosβ) * H2[i+i2+2]
                         - b7 * (1+cosβ) * H2[i+i2]
-                    )
+                    ) / 2
                     - a8 * sinβ * H2[i+i2+1]
                 )
+            end
+        end
+    end
 end
 
 
-function _step_4(d, n_max, mp_max, Hwedge, Hv):
+@inbounds function _step_4!(w::WignerWorkspace)
     """Recursively compute H^{m'+1, m}_{n}(β) for m'=1,...,n−1, m=m',...,n using relation (50) resolved
     with respect to H^{m'+1, m}_{n}:
 
@@ -207,9 +232,12 @@ function _step_4(d, n_max, mp_max, Hwedge, Hv):
     (where the last term drops out for m=n).
 
     """
-    if n_max > 0 and mp_max > 0:
-        for n in range(2, n_max+1):
-            for mp in range(1, min(n, mp_max)):
+    dvalues, n_max, mp_max = d(w.W), ℓₘₐₓ(w.W), m′ₘₐₓ(w.W)
+    Hwedge, Hv = w.Hwedge, w.Hv
+
+    if n_max > 0 && mp_max > 0
+        for n in 2:n_max
+            for mp in 1:min(n, mp_max)-1
                 # m = m', ..., n-1
                 # i1 = WignerHindex(n, mp+1, mp, mp_max)
                 i1 = WignerHindex(n, mp+1, mp+1, mp_max) - 1
@@ -219,34 +247,40 @@ function _step_4(d, n_max, mp_max, Hwedge, Hv):
                 i4 = WignerHindex(n, mp, mp+1, mp_max)
                 i5 = nm_index(n, mp)
                 i6 = nm_index(n, mp-1)
-                inverse_d5 = 1.0 / d[i5]
-                d6 = d[i6]
-                for i in [0]:
-                    d7 = d[i+i6]
-                    d8 = d[i+i5]
+                inverse_d5 = inv(dvalues[i5])
+                d6 = dvalues[i6]
+                for i in [0]
+                    d7 = dvalues[i+i6]
+                    d8 = dvalues[i+i5]
                     Hv[i+nm_index(n, mp+1)] = inverse_d5 * (
                           d6 * Hwedge[i+i2]
                         - d7 * Hv[i+nm_index(n, mp)]
                         + d8 * Hwedge[i+i4]
                     )
-                for i in range(1, n-mp):
-                    d7 = d[i+i6]
-                    d8 = d[i+i5]
+                end
+                for i in 1:n-mp-1
+                    d7 = dvalues[i+i6]
+                    d8 = dvalues[i+i5]
                     Hwedge[i+i1] = inverse_d5 * (
                           d6 * Hwedge[i+i2]
                         - d7 * Hwedge[i+i3]
                         + d8 * Hwedge[i+i4]
                     )
+                end
                 # m = n
-                for i in [n-mp]:
+                for i in [n-mp]
                     Hwedge[i+i1] = inverse_d5 * (
                           d6 * Hwedge[i+i2]
-                        - d[i+i6] * Hwedge[i+i3]
+                        - dvalues[i+i6] * Hwedge[i+i3]
                     )
+                end
+            end
+        end
+    end
 end
 
 
-function _step_5(d, n_max, mp_max, Hwedge, Hv):
+@inbounds function _step_5!(w::WignerWorkspace)
     """Recursively compute H^{m'−1, m}_{n}(β) for m'=−1,...,−n+1, m=−m',...,n using relation (50)
     resolved with respect to H^{m'−1, m}_{n}:
 
@@ -261,9 +295,12 @@ function _step_5(d, n_max, mp_max, Hwedge, Hv):
     also requires setting the (m',m)=(0,-1) components before beginning this loop.
 
     """
-    if n_max > 0 and mp_max > 0:
-        for n in range(0, n_max+1):
-            for mp in range(0, -min(n, mp_max), -1):
+    dvalues, n_max, mp_max = d(w.W), ℓₘₐₓ(w.W), m′ₘₐₓ(w.W)
+    Hwedge, Hv = w.Hwedge, w.Hv
+
+    if n_max > 0 && mp_max > 0
+        for n in 0:n_max
+            for mp in 0:-1:1-min(n, mp_max)
                 # m = -m', ..., n-1
                 # i1 = WignerHindex(n, mp-1, -mp, mp_max)
                 i1 = WignerHindex(n, mp-1, -mp+1, mp_max) - 1
@@ -276,35 +313,52 @@ function _step_5(d, n_max, mp_max, Hwedge, Hv):
                 i6 = nm_index(n, mp)
                 i7 = nm_index(n, -mp-1)
                 i8 = nm_index(n, -mp)
-                inverse_d5 = 1.0 / d[i5]
-                d6 = d[i6]
-                for i in [0]:
-                    d7 = d[i+i7]
-                    d8 = d[i+i8]
-                    if mp == 0:
+                inverse_d5 = inv(dvalues[i5])
+                d6 = dvalues[i6]
+                for i in [0]
+                    d7 = dvalues[i+i7]
+                    d8 = dvalues[i+i8]
+                    if mp == 0
                         Hv[i+nm_index(n, mp-1)] = inverse_d5 * (
                               d6 * Hv[i+nm_index(n, mp+1)]
                             + d7 * Hv[i+nm_index(n, mp)]
                             - d8 * Hwedge[i+i4]
                         )
-                    else:
+                    else
                         Hv[i+nm_index(n, mp-1)] = inverse_d5 * (
                               d6 * Hwedge[i+i2]
                             + d7 * Hv[i+nm_index(n, mp)]
                             - d8 * Hwedge[i+i4]
                         )
-                for i in range(1, n+mp):
-                    d7 = d[i+i7]
-                    d8 = d[i+i8]
+                    end
+                end
+                for i in 1:n+mp-1
+                    d7 = dvalues[i+i7]
+                    d8 = dvalues[i+i8]
                     Hwedge[i+i1] = inverse_d5 * (
                           d6 * Hwedge[i+i2]
                         + d7 * Hwedge[i+i3]
                         - d8 * Hwedge[i+i4]
                     )
+                end
                 # m = n
-                i = n+mp
-                Hwedge[i+i1] = inverse_d5 * (
-                      d6 * Hwedge[i+i2]
-                    + d[i+i7] * Hwedge[i+i3]
-                )
+                for i in [n+mp]
+                    Hwedge[i+i1] = inverse_d5 * (
+                          d6 * Hwedge[i+i2]
+                        + dvalues[i+i7] * Hwedge[i+i3]
+                    )
+                end
+            end
+        end
+    end
+end
+
+
+function H!(w::WignerWorkspace, expiβ::Complex)
+    _step_1!(w)
+    _step_2!(w, expiβ)
+    _step_3!(w, expiβ)
+    _step_4!(w)
+    _step_5!(w)
+    w.Hwedge, w.Hextra
 end
