9efc88168f747c940de1faa5b1350185b735fe65
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-09-20 12:08:12 -0400

    Add `stable` docs [skip tests]

--- docs/make.jl
+++ docs/make.jl
@@ -8,10 +8,7 @@ makedocs(
     format = Documenter.HTML(
         prettyurls = !("local" in ARGS),  # Use clean URLs, unless built as a "local" build
         edit_link = "main",  # Link out to "main" branch on github
-        #= UNCOMMENT ONCE STABLE:
         canonical = "https://moble.github.io/SphericalFunctions.jl/stable/",
-        warn_outdated = true,
-        :UNCOMMENT ONCE STABLE =#
     ),
     pages = [
         "Introduction" => "index.md",
