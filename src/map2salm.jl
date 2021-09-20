6cb5ca128cfbddd81fb6a1421482a2f55c42dffa
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-09-20 10:49:13 -0400

    Explain the map2salm `plan` object [skip tests]

--- src/map2salm.jl
+++ src/map2salm.jl
@@ -54,8 +54,14 @@ end
 Precompute values to use in executing [`map2salm`](@ref) or
 [`map2salm!`](@ref).
 
-The arguments to this function exactly mirror those of the first form
-[`map2salm`](@ref).
+The arguments to this function exactly mirror those of the first form of
+[`map2salm`](@ref), and all but the first argument in the first form of
+[`map2salm!`](@ref).  The `plan` returned by this function can be passed to the
+second forms of those functions to avoid some computation and allocation costs.
+
+Note that the `plan` object is not thread safe; a separate `plan` should be
+created for each thread that will use one, or locks should be used to ensure
+that a single `plan` is not used at the same time on different threads.
 
 """
 function plan_map2salm(map::AbstractArray{Complex{T}}, spin::Int, ℓmax::Int, ℓmin::Int=abs(spin)) where {T<:Real}
