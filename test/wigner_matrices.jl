3e9481de618d68f6a27337c5ceb39bcfe425b3cd
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-05-04 11:13:00 -0400

    Simplify use of test_utilities

--- test/wigner_matrices.jl
+++ test/wigner_matrices.jl
@@ -1,12 +1,5 @@
 @testset verbose=true "wigner_matrices" begin
-
-    # Some helpful definitions
-    include("test_utilities.jl")
-    using SphericalFunctions: WignerHsize, WignerDsize, WignerHindex, WignerDindex, abd
-
-    # Now the actual tests themselves
     include("wigner_matrices/small_d.jl")
     include("wigner_matrices/H.jl")
     include("wigner_matrices/big_D.jl")
-
 end
