5a73dd262d869bbbcbd1de024be2171213513d13
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-05-05 23:59:49 -0400

    Add NINJA formulas

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -168,7 +168,58 @@ function D_formula(n, m′, m, expiα::Complex{T}, expiβ::Complex{T}, expiγ::C
     return expiα^(m′) * d_formula(n, m′, m, expiβ) * expiγ^(m)
 end
 
+end  # module ExplicitWignerMatrices
+
+
+module NINJA
+
+function Wigner_d(ι::T, ell, m, s) where {T<:Real}
+    # Eq. II.8 of Ajith et al. (2007) 'Data formats...'
+    k_min = max(0, m - s)
+    k_max = min(ell + m, ell - s)
+    prefactor = √T(
+        factorial(big(ell + m))
+        * factorial(big(ell - m))
+        * factorial(big(ell + s))
+        * factorial(big(ell - s))
+    )
+    sum(
+        ifelse(iseven(k), 1, -1)
+         * cos(ι / 2) ^ (2 * ell + m - s - 2 * k)
+         * sin(ι / 2) ^ (2 * k + s - m)
+         * prefactor
+         / T(
+             factorial(big(ell + m - k))
+             * factorial(big(ell - s - k))
+             * factorial(big(k))
+             * factorial(big(k + s - m))
+         )
+        for k in k_min:k_max
+    )
 end
+
+function sYlm(s, ell, m, ι::T, ϕ::T) where {T<:Real}
+    # Eq. II.7 of Ajith et al. (2007) 'Data formats...'
+    # Note the weird definition w.r.t. `-s`
+    if abs(s) > ell || abs(m) > ell
+        return zero(complex(T))
+    end
+    (
+        ifelse(iseven(s), 1, -1)
+        * √((2ell + 1) / (4T(π)))
+        * Wigner_d(ι, ell, m, -s)
+        * cis(m * ϕ)
+    )
+end
+
+# Eqs. (II.9) through (II.13) of https://arxiv.org/abs/0709.0093v3
+m2Y22(ι::T, ϕ::T) where {T<:Real} = √(5 / (64T(π))) * (1 + cos(ι))^2 * cis(2ϕ)
+m2Y21(ι::T, ϕ::T) where {T<:Real} = √(5 / (16T(π))) * sin(ι) * (1 + cos(ι)) * cis(ϕ)
+m2Y20(ι::T, ϕ::T) where {T<:Real} = √(15 / (32T(π))) * sin(ι)^2
+m2Y2m1(ι::T, ϕ::T) where {T<:Real} = √(5 / (16T(π))) * sin(ι) * (1 - cos(ι)) * cis(-1ϕ)
+m2Y2m2(ι::T, ϕ::T) where {T<:Real} = √(5 / (64T(π))) * (1 - cos(ι))^2 * cis(-2ϕ)
+
+end  # module NINJA
 
 
 αrange(::Type{T}, n=15) where T = T[
