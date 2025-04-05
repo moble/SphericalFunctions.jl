9913d7394d9134d149874e6624245ece73388a3f
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-04-05 09:54:47 -0400

    Explain arguments to Literate.markdown

--- docs/make.jl
+++ docs/make.jl
@@ -22,13 +22,21 @@ literate_output = joinpath(docs_src_dir, "literate_output")
 relative_literate_output = relpath(literate_output, docs_src_dir)
 relative_convention_comparisons = joinpath(relative_literate_output, "conventions_comparisons")
 rm(literate_output; force=true, recursive=true)
-skip_files = (  # Non-.jl files will be skipped anyway
+skip_input_files = (  # Non-.jl files will be skipped anyway
     "ConventionsUtilities.jl",
     "ConventionsSetup.jl",
 )
+# I am writing these specifically to be consumed by Documenter; setting this option
+# enables lots of nice conversions.
+documenter=true
+# To support markdown strings, as in md""" ... """, we need to set this option.
+mdstrings=true
+# We *don't* want to execute the code in the literate script, because they are meant to
+# be used with TestItems.jl, and we don't want to run the tests here.
+execute=false
 for (root, _, files) ∈ walkdir(literate_input), file ∈ files
     # Skip some files
-    if splitext(file)[2] != ".jl" || file ∈ skip_files
+    if splitext(file)[2] != ".jl" || file ∈ skip_input_files
         continue
     end
     # full path to a literate script
@@ -36,7 +44,7 @@ for (root, _, files) ∈ walkdir(literate_input), file ∈ files
     # generated output path
     output_path = splitdir(replace(input_path, literate_input=>literate_output))[1]
     # generate the markdown file calling Literate
-    Literate.markdown(input_path, output_path, documenter=true, mdstrings=true)
+    Literate.markdown(input_path, output_path; documenter, mdstrings, execute)
 end
 
 # Make "lalsuite_SphericalHarmonics.c" available in the docs
