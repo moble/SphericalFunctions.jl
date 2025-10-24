f32fd45721f02218d7b1aef24e6275fb1b88c849
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-10-24 14:55:37 -0400

    Make EncodeDecode utility a little more robust

--- test/utilities/encoder.jl
+++ test/utilities/encoder.jl
@@ -4,16 +4,19 @@ integer, and decoding them back again.  This is useful for storing those integer
 structures that only accept single integers or floats, such as HWedge.
 """
 @testmodule EncodeDecode begin
-    encode(i::Int) = i
-    encode(i::Rational) = numerator(i)
+    encode(i::Int) = i + 50
+    encode(i::Rational) = numerator(i) + 50
     function encode(i...)
         output = 0
+        max_integers = log10(typemax(output)) ÷ 2
+        if length(i) > max_integers
+            error("Can only encode up to $max_integers integers into a single integer")
+        end
         for (j, v) in enumerate(i)
-            if abs(v) >= 10
-                error("Can only encode integers with absolute value less than 10")
+            if !(-50 ≤ v ≤ 49)
+                error("Can only encode integers in -50:49; got $v")
             end
-            output += (v < 0) * 10^(2j-2)
-            output += encode(abs(v)) * 10^(2j-1)
+            output += encode(v) * 10^(2j-2)
         end
         output
     end
@@ -21,8 +24,8 @@ structures that only accept single integers or floats, such as HWedge.
     function decode(i; pad=2)
         d = digits(i; pad)
         output = Int[]
-        for k in 1:(length(d) ÷ 2)
-            v = d[2k] * (d[2k-1] == 1 ? -1 : 1)
+        for k in 1:2:length(d)
+            v = (d[k] + 10d[k+1]) - 50
             push!(output, v)
         end
         return Tuple(output)
@@ -32,18 +35,19 @@ end
 @testitem "EncodeDecode" setup=[EncodeDecode] begin
     using .EncodeDecode: encode, decode
     for ℓₘₐₓ ∈ (5, 7//2)
-    for iᵣ ∈ 1:7
-        for m′ ∈ -ℓₘₐₓ:ℓₘₐₓ
-            for m ∈ -ℓₘₐₓ:ℓₘₐₓ
-                code = encode(iᵣ, m′, m)
-                (iᵣ₂, m′₂, m₂) = decode(code; pad=6)
-                @test iᵣ == iᵣ₂
-                if ℓₘₐₓ isa Int
-                    @test m′ == m′₂
-                    @test m == m₂
-                else
-                    @test m′ == m′₂ // 2
-                    @test m == m₂ // 2
+        for iᵣ ∈ 1:7
+            for m′ ∈ -ℓₘₐₓ:ℓₘₐₓ
+                for m ∈ -ℓₘₐₓ:ℓₘₐₓ
+                    code = encode(iᵣ, m′, m)
+                    (iᵣ₂, m′₂, m₂) = decode(code; pad=6)
+                    @test iᵣ == iᵣ₂
+                    if ℓₘₐₓ isa Int
+                        @test m′ == m′₂
+                        @test m == m₂
+                    else
+                        @test m′ == m′₂ // 2
+                        @test m == m₂ // 2
+                    end
                 end
             end
         end
