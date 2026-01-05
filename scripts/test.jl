b5f9e93bdda361a8457f40856e31710f6507bf5a
Mike Boyle <michael.oliver.boyle@gmail.com>
2026-01-05 09:56:36 -0500

    Require `--coverage` flag to run coverage

--- scripts/test.jl
+++ scripts/test.jl
@@ -1,5 +1,7 @@
 # Call this from the top-level directory as
 #   julia -t auto scripts/test.jl
+# or to run with coverage as
+#   julia -t auto scripts/test.jl --coverage
 # See docs/src/development/index.md for more information.
 
 import Dates
@@ -13,16 +15,25 @@ using LinearAlgebra
 using Base.Threads
 LinearAlgebra.BLAS.set_num_threads(nthreads())
 
+# Check for the `--coverage` flag
+coverage = any(ARGS .== "--coverage")
+
 try
-    Δt = @elapsed Pkg.test("SphericalFunctions"; coverage=true, test_args=ARGS)
+    Δt = @elapsed Pkg.test("SphericalFunctions"; coverage, test_args=ARGS)
     println("Running tests took $Δt seconds.")
 catch e
-    println("Tests failed; proceeding to coverage")
+    if coverage
+        println("Tests failed; proceeding to coverage")
+    else
+        println("Tests failed.")
+    end
 end
 
-Pkg.activate()  # Activate Julia's base (home) directory
-using Coverage
-cd((@__DIR__) * "/..")
-coverage = Coverage.process_folder("src")
-Coverage.writefile("lcov.info", coverage)
-Coverage.clean_folder(".")
+if coverage
+    Pkg.activate()  # Activate Julia's base (home) directory
+    using Coverage
+    cd((@__DIR__) * "/..")
+    coverage = Coverage.process_folder("src")
+    Coverage.writefile("lcov.info", coverage)
+    Coverage.clean_folder(".")
+end
